c06739f92e9fb462994ff1b059644a17e14f6605
Daniel Marques <up201503822@fe.up.pt>
2019-03-16 16:07:41 +0000

    Initial menu

---
+++ Preliminary Project/Assets/Scripts/UserInterface.cs
@@ -0,0 +1,9 @@
+// This script handles detecting collisions with traps and telling the Game Manager
+// when the player dies
+
+using UnityEngine;
+
+public class UserInterface : MonoBehaviour
+{
+    
+}
