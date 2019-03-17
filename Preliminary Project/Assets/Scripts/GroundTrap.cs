9374f0d3ee4d9ae42e544971c47282e56f9b4d0a
João Seixas <up201505648@fe.up.pt>
2019-03-17 14:57:29 +0000

    Falling Trap

--- Preliminary Project/Assets/Scripts/GroundTrap.cs
+++ Preliminary Project/Assets/Scripts/GroundTrap.cs
@@ -5,75 +5,43 @@ using UnityEngine;
 public class GroundTrap : MonoBehaviour
 {
     int playerLayer;
-    int platformsLayer;
-    public float timeToFell;
-    public float fellTime;
-    bool deleteSides;
-    bool hasFell;
-    float previousGravity;
-    public float gravityScale = 0.2f;
-    Rigidbody2D rigidBody;
-    Collider2D triggerBox;
+    public float timeToDestroy;
+    bool toDestroy;
 
     void Start()
     {
-        hasFell = false;
-        deleteSides = true;
+        toDestroy = false;
         playerLayer = LayerMask.NameToLayer("Player");
-        rigidBody = GetComponent<Rigidbody2D>();
-
-        previousGravity = rigidBody.gravityScale;
-        rigidBody.isKinematic = true;
-        rigidBody.gravityScale = 0f;
-        rigidBody.mass = 100f;
     }
 
     void Update()
     {
 
-        Debug.Log(Time.deltaTime);
-        if (hasFell) {
-            if(timeToFell > 0)
-                timeToFell -= Time.deltaTime;
+        //If player collided with it
+        if (toDestroy) {
+
+            //Check time between first contact of the player and the time to destroy the platform
+            if(timeToDestroy > 0)
+                timeToDestroy -= Time.deltaTime;
             else
-            {
-                if(deleteSides)
-                {
-                    deleteSides = false;
-                    //Remove sides
-                    Destroy(GetComponent<Transform>().GetChild(2).gameObject);
-                    Destroy(GetComponent<Transform>().GetChild(2).gameObject);
-
-                    rigidBody.isKinematic = false;
-
-                    rigidBody.gravityScale = previousGravity * gravityScale;
-
-                    Debug.Log("Platform falling");
-                }
-
-                if (fellTime > 0)
-                    fellTime -= Time.deltaTime;
-                else
-                    Destroy(gameObject);
-            }
+                Destroy(gameObject);
         }
+
     }
 
-    void OnTriggerEnter2D(Collider2D collision)
+    void OnCollisionEnter2D(Collision2D collision)
     {
-        Debug.Log("1  Platform falling");
-        //Only start falling if the trigger collision is with the player 
+       
+        //Only destroy if the collision is with the player 
         if (collision.gameObject.layer != playerLayer)
             return;
-        Debug.Log("222 Platform falling");
-        //Disable subsequent collisions
-        if (hasFell)
-            return;
-        Debug.Log("33 Platform falling");
         
+        //Disable subsequent collisions
+        if (toDestroy)
+            return;
 
-        //Let it fall
-        hasFell = true;
+        //Enable destruction
+        toDestroy = true;
     }
 
 
