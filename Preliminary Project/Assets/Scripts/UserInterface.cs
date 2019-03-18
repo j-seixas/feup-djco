1cb3fa275f25a057375f2ff9f7a3837a85b633d6
Daniel Marques <up201503822@fe.up.pt>
2019-03-18 19:00:54 +0000

    Added story screens!

--- Preliminary Project/Assets/Scripts/UserInterface.cs
+++ Preliminary Project/Assets/Scripts/UserInterface.cs
@@ -3,6 +3,20 @@ using UnityEngine.SceneManagement;
 
 public class UserInterface : MonoBehaviour
 {
+	public GameObject canvas;
+	public GameObject storyCanvas;
+	public GameObject startStory;
+	public GameObject endStory;
+	static private bool end = false;
+
+	void Update()
+	{
+		if(end) {
+			ShowEndStory();
+			end = false;
+		}
+	}
+
 	public void PlayGame()
 	{
 		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
@@ -16,5 +30,34 @@ public class UserInterface : MonoBehaviour
 	public void SetVolume(float volume)
 	{
 		SoundManager.SetVolume(volume);
+	}
+
+	public void ShowStartStory()
+	{
+		canvas.SetActive(false);
+		storyCanvas.SetActive(true);
+		startStory.SetActive(true);
+		endStory.SetActive(false);
+	}
+
+	public void ShowEndStory()
+	{
+		canvas.SetActive(false);
+		storyCanvas.SetActive(true);
+		startStory.SetActive(false);
+		endStory.SetActive(true);
+	}
+
+	public void DisplayMenu()
+	{
+		canvas.SetActive(true);
+		storyCanvas.SetActive(false);
+		startStory.SetActive(false);
+		endStory.SetActive(false);
+	}
+
+	static public void OnGameOver()
+	{
+		end = true;
 	}
 }
